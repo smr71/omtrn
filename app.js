4f42b73463c482cd4cc31f57497e5e6df9b44bb6
iocaste <59840991+iocaste@users.noreply.github.com>
2021-08-16 14:19:40 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+/*
+
+Ideas:
+
+General repeat timer below main panel
+Settings to adjust amount of time
+When timer runs out, give compulsory action
+- If desp value is low, more chance of pee 
+- Additional drink, always some chance but decreased as desp rises
+- If not pee, random chance of other challenges (lie on stomach, jumping jacks, stand in bathrm)
+
 function now() {
 	return Date.now()/60000;
 }
